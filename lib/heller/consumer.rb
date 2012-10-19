70d1ab42180ecc3e0076c769ec69ccd085ceb573
Mathias Söderberg <mathias.soederberg@gmail.com>
2012-10-19 19:20:54 +0200

    Added specs and implementation for multi_fetch (multifetch in Kafka Api)

--- lib/heller/consumer.rb
+++ lib/heller/consumer.rb
@@ -14,23 +14,39 @@ module Heller
 			single_offset(topic, partition, EARLIEST_OFFSET)
 		end
 
-		def fetch_request(topic, partition, offset, max_size = MAX_FETCH_SIZE)
+		def fetch_request(topic, partition, offset, max_size)
 			Kafka::Api::FetchRequest.new(topic, partition, offset, max_size)
 		end
 
-		def fetch(fetch_request)
-			messages = super
+		def consume(topic, partition, offset, max_size = MAX_FETCH_SIZE)
+			request = fetch_request(topic, partition, offset, max_size)
+
+			messages = fetch(request)
 			messages.to_a
 		end
 
-		private
+		def multi_fetch(topics_hash, max_size = MAX_FETCH_SIZE)
+			requests = topics_hash.map { |topic, hash| fetch_request(topic, hash[:partition], hash[:offset], max_size) }
+
+			response = multifetch(java.util.ArrayList.new(requests))
+			parse_multi_fetch_response(topics_hash.keys, response)
+		end
+
+		protected
 
 		def single_offset(topic, partition, time)
 			offsets = get_offsets_before(topic, partition, time, 1)
 			offsets = offsets.to_a
 
-			unless offsets.empty?
-				offsets.first
+			offsets.first unless offsets.empty?
+		end
+
+		def parse_multi_fetch_response(topics, response)
+			response_array = topics.zip(response.to_a)
+
+			response_array.inject({}) do |response_hash, (topic, messages)| 
+				response_hash[topic] = messages 
+				response_hash
 			end
 		end
 	end
--- spec/heller/consumer_spec.rb
+++ spec/heller/consumer_spec.rb
@@ -6,7 +6,7 @@ module Heller
 
 		let(:consumer) { Consumer.new('localhost', 9092, 10000, 1024000) }
 
-		context 'added offset methods' do
+		context 'offset methods' do
 
 			before do
 				consumer.stub(:get_offsets_before).and_return(offsets)
@@ -51,24 +51,18 @@ module Heller
 				fetch_request.offset.should eq(offset)
 				fetch_request.max_size.should eq(max_size)
 			end
-
-			it 'should set default max fetch size if none given' do
-				fetch_request = consumer.fetch_request(topic, partition, offset)
-
-				fetch_request.max_size.should eq(Consumer::MAX_FETCH_SIZE)
-			end
 		end
 
-		describe '#fetch' do
+		describe '#consume' do
 
-			let(:fetch_request) { consumer.fetch_request('0', 0, 0) }
+			let(:topic) { '0' }
+			let(:partition) { 0 }
+			let(:offset) { 0 }
 
-			before do
-				Kafka::Consumer::SimpleConsumer.any_instance.stub(:fetch).and_return(java.util.ArrayList.new)
-			end
+			it 'should return an array' do
+				consumer.should_receive(:fetch).with(kind_of(Kafka::Api::FetchRequest)).and_return(java.util.ArrayList.new)
 
-			it 'should return an array', :pending => true do
-				fetched = consumer.fetch(fetch_request)
+				fetched = consumer.consume(topic, partition, offset)
 
 				fetched.should be_kind_of(Array)
 				fetched.should be_empty
@@ -76,5 +70,55 @@ module Heller
 
 		end
 
+		describe '#multi_fetch' do
+
+			let(:topics_hash) do 
+				{
+					'0' => {
+						:partition => 0,
+						:offset => 0
+					},
+					'1' => {
+						:partition => 0,
+						:offset => 0
+					},
+					'2' => {
+						:partition => 0,
+						:offset => 0
+					}
+				}
+			end
+
+			let(:multi_fetch_response) { mock('MultiFetchResponse') }
+			let(:response) do
+
+			end
+			let(:expected_hash) do
+				{
+					'0' => [],
+					'1' => [],
+					'2' => []
+				}
+			end
+
+			before do
+				multi_fetch_response.stub(:to_a).and_return([[], [], []])
+			end
+
+			it 'should return a hash of topics and messages' do
+				consumer.should_receive(:multifetch).with(kind_of(java.util.ArrayList)).and_return(multi_fetch_response)
+
+				fetch_hash = consumer.multi_fetch(topics_hash)
+
+				fetch_hash.should be_kind_of(Hash)
+				fetch_hash.keys.should eq(topics_hash.keys)
+
+				fetch_hash.values.each do |value|
+					value.should be_kind_of(Array)
+				end
+
+				fetch_hash.should eq(expected_hash)
+			end
+		end
 	end
 end

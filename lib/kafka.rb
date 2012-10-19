780b64150887459ee7b2892a3aa0c7dfe57dac95
Mathias Söderberg <mathias.soederberg@gmail.com>
2012-10-19 19:25:47 +0200

    Removed unnecessary imports (for the time being)

--- lib/kafka.rb
+++ lib/kafka.rb
@@ -8,38 +8,10 @@ module Kafka
 		java_import 'kafka.api.FetchRequest'
 		java_import 'kafka.api.MultiFetchRequest'
 		java_import 'kafka.api.MultiFetchResponse'
-		java_import 'kafka.api.MultiProducerRequest'
-		java_import 'kafka.api.OffsetRequest'
-		java_import 'kafka.api.ProducerRequest'
 	end
 
 	module Consumer
 		java_import 'kafka.javaapi.consumer.SimpleConsumer'
-
-		class SimpleConsumer
-			
-			LATEST_OFFSET = -1
-			EARLIEST_OFFSET = -2
-
-			def latest_offset(topic, partition)
-				single_offset(topic, partition, LATEST_OFFSET)
-			end
-
-			def earliest_offset(topic, partition)
-				single_offset(topic, partition, EARLIEST_OFFSET)
-			end
-
-			private
-
-			def single_offset(topic, partition, time)
-				offsets = get_offsets_before(topic, partition, time, 1)
-				offsets = offsets.to_a
-
-				unless offsets.empty?
-					offsets.first
-				end
-			end
-		end
 	end
 
 	module Message

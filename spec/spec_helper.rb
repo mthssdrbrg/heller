4c15f6083cab3028b368d5954776970b5b89e7ea
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-07-15 23:44:07 +0200

    Add support Fakers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 # encoding: utf-8
 
 require 'heller'
+require 'spec/support/fakers'
+
+RSpec.configure do |config|
+  config.include(Fakers)
+end
---
+++ spec/support/fakers.rb
@@ -0,0 +1,44 @@
+module Fakers
+  class FakeFetchResponse < Struct.new(:topic, :partition, :messages)
+    def message_set(topic, partition)
+      if topic == self.topic && partition == self.partition
+        messages
+      end
+    end
+  end
+
+  FakeMessageAndOffset = Struct.new(:message, :offset)
+  FakeMessage = Struct.new(:payload)
+
+  class FakePayload
+    def initialize(payload)
+      @payload = payload.to_java_bytes
+    end
+
+    def limit
+      @payload.size
+    end
+
+    def get(buffer)
+      @payload.each_with_index do |b, i|
+        buffer[i] = b
+      end
+    end
+  end
+
+  def create_fake_message(payload)
+    FakeMessage.new(FakePayload.new(payload))
+  end
+
+  def create_fake_fetch_response(*payloads)
+    message_set = create_fake_message_set(*payloads)
+    FakeFetchResponse.new('spec', 0, message_set)
+  end
+
+  def create_fake_message_set(*payloads)
+    payloads.each_with_index.map do |payload, index|
+      message = create_fake_message(payload)
+      FakeMessageAndOffset.new(message, index)
+    end
+  end
+end

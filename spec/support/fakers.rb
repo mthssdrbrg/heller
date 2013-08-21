9c3619ec9d9a76c841cb84a4e2711d1577bb1710
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-21 19:22:31 +0200

    Remove unnecessary spec 'fakers'

--- spec/support/fakers.rb
+++ spec/support/fakers.rb
@@ -1,12 +1,4 @@
 module Fakers
-  class FakeFetchResponse < Struct.new(:topic, :partition, :messages)
-    def message_set(topic, partition)
-      if topic == self.topic && partition == self.partition
-        messages
-      end
-    end
-  end
-
   FakeMessageAndOffset = Struct.new(:message, :offset)
   FakeMessage = Struct.new(:payload)
 
@@ -28,11 +20,6 @@ module Fakers
 
   def create_fake_message(payload)
     FakeMessage.new(FakePayload.new(payload))
-  end
-
-  def create_fake_fetch_response(*payloads)
-    message_set = create_fake_message_set(*payloads)
-    FakeFetchResponse.new('spec', 0, message_set)
   end
 
   FakeMessageSet = Struct.new(:iterator)

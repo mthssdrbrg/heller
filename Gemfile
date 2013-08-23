44cbda21b4d98060071043038b33a7cfc6fe5364
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-24 01:30:47 +0200

    Add coveralls

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem 'kafka-jars', git: 'git://github.com/mthssdrbrg/kafka-jars.git'
 group :test do
   gem 'rspec'
   gem 'simplecov', require: false
+  gem 'coveralls', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,24 @@
 # encoding: utf-8
 
-require 'simplecov'
-SimpleCov.start
-
-require 'heller'
 require 'spec/support/fakers'
 
 RSpec.configure do |config|
   config.include(Fakers)
+  config.order = 'random'
 end
+
+require 'coveralls'
+require 'simplecov'
+
+if ENV.include?('TRAVIS')
+  Coveralls.wear!
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+end
+
+SimpleCov.start do
+  add_group 'Source', 'lib'
+  add_group 'Unit tests', 'spec/heller'
+  add_group 'Integration tests', 'spec/integration'
+end
+
+require 'heller'

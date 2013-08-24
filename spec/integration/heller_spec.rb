3304dc8fb4d0e024e39b3593e0c396d228ee2560
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-24 02:16:01 +0200

    Separate integration specs for Producer and Consumer

--- spec/integration/heller_spec.rb
+++ spec/integration/heller_spec.rb
@@ -14,33 +14,75 @@ module Heller
       Consumer.new('localhost:9092', client_id: 'spec-consumer')
     end
 
-    context 'simple string messages' do
-      let :topic do
-        "spec-simple-string-#{Time.now.to_i.to_s(36)}"
+    context 'Producer' do
+      context 'without an explicit key' do
+        let :topic do
+          "spec-without-explicit-key-#{Time.now.to_i.to_s(36)}"
+        end
+
+        it 'is able to push messages' do
+          expect { producer.push(Heller::Message.new(topic, 'simple string message')) }.not_to raise_error
+        end
       end
 
-      it 'handles simple string messages' do
-        producer.push(Heller::Message.new(topic, 'simple string message'))
+      context 'with an explicit key' do
+        let :topic do
+          "spec-with-explicit-key-#{Time.now.to_i.to_s(36)}"
+        end
 
-        fetch_response = consumer.fetch(FetchRequest.new(topic, 0, 0))
-        enumerator = fetch_response.messages(topic, 0)
-        enumerator.should be_a(MessageSetEnumerator)
+        it 'is able to push messages' do
+          expect { producer.push(Heller::Message.new(topic, 'simple string message', 'some-key')) }.not_to raise_error
+        end
+      end
+    end
 
-        messages = enumerator.to_a
-        message = messages.last.last
-        message.should eq('simple string message')
+    context 'Consumer' do
+      let :fetch_response do
+        consumer.fetch(FetchRequest.new(topic, 0, 0))
       end
 
-      it 'handles JSON hashes' do
-        producer.push(Heller::Message.new(topic, {'a key' => 'a value'}.to_json))
+      context 'simple string messages' do
+        let :topic do
+          "spec-simple-string-#{Time.now.to_i.to_s(36)}"
+        end
 
-        fetch_response = consumer.fetch(FetchRequest.new(topic, 0, 0))
-        enumerator = fetch_response.messages(topic, 0)
-        enumerator.should be_a(MessageSetEnumerator)
+        before do
+          producer.push(Heller::Message.new(topic, 'simple string message'))
+        end
 
-        messages = enumerator.to_a
-        message = messages.last.last
-        expect(JSON.parse(message)).to eq({'a key' => 'a value'})
+        it 'is no big deal' do
+          enumerator = fetch_response.messages(topic, 0)
+          enumerator.should be_a(MessageSetEnumerator)
+
+          messages = enumerator.to_a
+          expect(messages).to have(1).item
+
+          offset, message = messages.last
+          expect(offset).to be_zero
+          expect(message).to eq('simple string message')
+        end
+      end
+
+      context 'JSON serialized hashes' do
+        let :topic do
+          "spec-json-hash-#{Time.now.to_i.to_s(36)}"
+        end
+
+        before do
+          producer.push(Heller::Message.new(topic, {'a key' => 'a value'}.to_json))
+        end
+
+        it 'is no big deal' do
+          enumerator = fetch_response.messages(topic, 0)
+          enumerator.should be_a(MessageSetEnumerator)
+
+          messages = enumerator.to_a
+          expect(messages).to have(1).item
+
+          offset, message = messages.last
+          expect(offset).to be_zero
+          expect(JSON.parse(message)).to eq({'a key' => 'a value'})
+        end
       end
     end
   end

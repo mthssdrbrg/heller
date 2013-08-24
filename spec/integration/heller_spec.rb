6a98f780ede5bd18a0f32096155b2776b010d99c
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-24 02:40:40 +0200

    Add integration specs for Consumer#metadata
    Also includes pending blocks for #offsets_before,
    #earliest_offset and #latest_offset.
    Think I have to create some form of wrapper / proxy
    class for TopicMetadataResponse though.

--- spec/integration/heller_spec.rb
+++ spec/integration/heller_spec.rb
@@ -37,52 +37,108 @@ module Heller
     end
 
     context 'Consumer' do
-      let :fetch_response do
-        consumer.fetch(FetchRequest.new(topic, 0, 0))
+      describe '#fetch' do
+        let :fetch_response do
+          consumer.fetch(FetchRequest.new(topic, 0, 0))
+        end
+
+        context 'simple string messages' do
+          let :topic do
+            "spec-simple-string-#{Time.now.to_i.to_s(36)}"
+          end
+
+          before do
+            producer.push(Heller::Message.new(topic, 'simple string message'))
+          end
+
+          it 'is no big deal' do
+            enumerator = fetch_response.messages(topic, 0)
+            enumerator.should be_a(MessageSetEnumerator)
+
+            messages = enumerator.to_a
+            expect(messages).to have(1).item
+
+            offset, message = messages.last
+            expect(offset).to be_zero
+            expect(message).to eq('simple string message')
+          end
+        end
+
+        context 'JSON serialized hashes' do
+          let :topic do
+            "spec-json-hash-#{Time.now.to_i.to_s(36)}"
+          end
+
+          before do
+            producer.push(Heller::Message.new(topic, {'a key' => 'a value'}.to_json))
+          end
+
+          it 'is no big deal' do
+            enumerator = fetch_response.messages(topic, 0)
+            enumerator.should be_a(MessageSetEnumerator)
+
+            messages = enumerator.to_a
+            expect(messages).to have(1).item
+
+            offset, message = messages.last
+            expect(offset).to be_zero
+            expect(JSON.parse(message)).to eq({'a key' => 'a value'})
+          end
+        end
       end
 
-      context 'simple string messages' do
-        let :topic do
-          "spec-simple-string-#{Time.now.to_i.to_s(36)}"
-        end
-
+      describe '#metadata' do
         before do
-          producer.push(Heller::Message.new(topic, 'simple string message'))
+          topics.each { |topic| producer.push(Heller::Message.new(topic, 'metadata request message')) }
         end
 
-        it 'is no big deal' do
-          enumerator = fetch_response.messages(topic, 0)
-          enumerator.should be_a(MessageSetEnumerator)
+        context 'when given a single topic' do
+          let :topics do
+            ["spec-single-metadata-topic-#{Time.now.to_i.to_s(36)}"]
+          end
 
-          messages = enumerator.to_a
-          expect(messages).to have(1).item
+          # TODO: add some form of wrapper class, or something, it's seems like a bother to manage
+          # TopicMetadata objects "manually".
 
-          offset, message = messages.last
-          expect(offset).to be_zero
-          expect(message).to eq('simple string message')
+          it 'returns metadata about given topic' do
+            response = consumer.metadata(topics)
+            metadata = response.topics_metadata
+
+            expect(metadata).to have(1).item
+
+            topic_metadata = metadata.first
+            expect(topic_metadata.topic).to eq(topics.first)
+          end
+        end
+
+        context 'when given several topics' do
+          let :topics do
+            [1, 2, 3].map { |i| "spec-multiple-metadata-topics-#{i}-#{Time.now.to_i.to_s(36)}" }
+          end
+
+          it 'returns metadata about given topics' do
+            response = consumer.metadata(topics)
+            metadata = response.topics_metadata
+
+            expect(metadata).to have(3).item
+
+            topics.zip(metadata).each do |topic, metadata|
+              expect(metadata.topic).to eq(topic)
+            end
+          end
         end
       end
 
-      context 'JSON serialized hashes' do
-        let :topic do
-          "spec-json-hash-#{Time.now.to_i.to_s(36)}"
-        end
+      describe '#offsets_before' do
+        pending
+      end
 
-        before do
-          producer.push(Heller::Message.new(topic, {'a key' => 'a value'}.to_json))
-        end
+      describe '#earliest_offset' do
+        pending
+      end
 
-        it 'is no big deal' do
-          enumerator = fetch_response.messages(topic, 0)
-          enumerator.should be_a(MessageSetEnumerator)
-
-          messages = enumerator.to_a
-          expect(messages).to have(1).item
-
-          offset, message = messages.last
-          expect(offset).to be_zero
-          expect(JSON.parse(message)).to eq({'a key' => 'a value'})
-        end
+      describe '#latest_offset' do
+        pending
       end
     end
   end

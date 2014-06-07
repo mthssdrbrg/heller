bf9b6ccd519cbce9983787611757337b9341139b
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-06-07 02:36:02 +0200

    Split modules / classes from `lib/heller.rb` to separate files

--- lib/heller.rb
+++ lib/heller.rb
@@ -9,48 +9,6 @@ module Heller
   java_import 'java.lang.IllegalArgumentException'
   java_import 'java.util.NoSuchElementException'
 
-  class Message < Kafka::Producer::KeyedMessage
-    def initialize(topic, message, key = nil)
-      super(topic, key, message)
-    end
-  end
-
-  class FetchRequest
-    attr_reader :topic, :partition, :offset
-
-    def initialize(*args)
-      @topic, @partition, @offset = *args
-    end
-  end
-
-  class OffsetRequest
-    attr_reader :topic, :partition, :time, :max_offsets
-
-    def self.latest_time
-      Kafka::Api::OffsetRequest.latest_time
-    end
-
-    def self.earliest_time
-      Kafka::Api::OffsetRequest.earliest_time
-    end
-
-    def self.current_version
-      Kafka::Api::OffsetRequest.current_version
-    end
-
-    def initialize(topic, partition, time, offsets = 1)
-      @topic, @partition, @time, @max_offsets = topic, partition, time, offsets
-    end
-  end
-
-  Errors = Kafka::Errors::ErrorMapping
-
-  class Errors
-    def self.error_for(code)
-      self.exception_for(code)
-    end
-  end
-
   module Concurrency
     java_import 'java.util.concurrent.locks.ReentrantLock'
 
@@ -73,13 +31,17 @@ module Heller
 end
 
 require 'heller/configuration'
+require 'heller/consumer_configuration'
 require 'heller/producer'
 require 'heller/producer_configuration'
-require 'heller/message_set_enumerator'
+require 'heller/errors'
+require 'heller/fetch_request'
 require 'heller/fetch_response'
 require 'heller/topic_metadata_response'
+require 'heller/message'
+require 'heller/message_set_enumerator'
+require 'heller/offset_request'
 require 'heller/offset_response'
 require 'heller/consumer'
-require 'heller/version'
-require 'heller/consumer_configuration'
 require 'heller/zookeeper_consumer'
+require 'heller/version'

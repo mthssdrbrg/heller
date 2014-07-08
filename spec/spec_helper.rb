59fb5d91d7b255c143e3a530c8be23baf9ef1766
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-07-08 21:29:39 +0200

    Silence log4j during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,20 @@ RSpec.configure do |config|
   config.include(Fakers)
   config.order = 'random'
   config.raise_errors_for_deprecations!
+  config.before(:suite) do
+    unless java.lang.System.getProperty('log4j.configuration')
+      @log4j_config_file = Tempfile.new('silent-log4j.properties')
+      @log4j_config_file.puts('log4j.rootLogger=OFF')
+      @log4j_config_file.rewind
+      java.lang.System.setProperty('log4j.configuration', %(file://#{@log4j_config_file.path}))
+    end
+  end
+  config.after(:suite) do
+    if @log4j_config_file
+      @log4j_config_file.close
+      @log4j_config_file.unlink
+    end
+  end
 end
 
 require 'coveralls'
